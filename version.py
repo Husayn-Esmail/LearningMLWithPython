9adf9fcb73a2f3f96741e5e7bd58e2b2670ac2bb
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2022-05-17 11:07:41 -0600

    created file that checks version of libraries

---
+++ version.py
@@ -0,0 +1,21 @@
+# checks the version of libraries
+
+# Python version
+import sys
+print("Python: {}".format(sys.version))
+# SciPy
+import scipy
+print("SciPy: {}".format(scipy.__version__))
+# NumPy
+import numpy
+print("NumPy: {}".format(numpy.__version__))
+# matplotlib
+import matplotlib
+print("matplotlib: {}".format(matplotlib.__version__))
+# pandas
+import pandas
+print("pandas: {}".format(pandas.__version__))
+# scikit-learn
+import sklearn
+print("sklearn: {}".format(sklearn.__version__))
+
